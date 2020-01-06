e7acbd81cc03a47fbf539ddb56e126d35f6e53bc
Rohithgilla12 <gillarohith1@gmail.com>
2020-01-06 23:27:55 +0530

    Initial Commit 🔥

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:lost_inator/screens/signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  static final String id = "loginScreen";
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+  _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      print("Email is  $_email and password is $_password");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Locatinator",
+            style: TextStyle(
+                fontSize: 32.0,
+                color: Colors.brown,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Image(
+            image: AssetImage("assets/images/ctrlf.png"),
+            height: 65.0,
+          ),
+          Image(
+            image: AssetImage("assets/images/panda.png"),
+            height: 220.0,
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 10.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      icon: Icon(Icons.mail_outline),
+                    ),
+                    validator: (input) => !input.contains("@")
+                        ? "Please input valid email id"
+                        : null,
+                    onSaved: (input) => _email = input,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                    vertical: 10.0,
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      icon: Icon(Icons.vpn_key),
+                    ),
+                    validator: (input) =>
+                        input.length < 6 ? "Check your password field" : null,
+                    onSaved: (input) => _password = input,
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: 250.0,
+                  child: FlatButton(
+                    onPressed: _submit,
+                    color: Colors.brown[300],
+                    child: Text("Login"),
+                  ),
+                ),
+                Container(
+                  width: 250.0,
+                  child: FlatButton(
+                    onPressed: () =>
+                        Navigator.pushNamed(context, SignupScreen.id),
+                    color: Colors.brown[300],
+                    child: Text("Sign up"),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
